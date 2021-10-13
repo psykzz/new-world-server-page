670c125ed888f083247e225ffbc6f4b6f19b4e84
Matt Smith <matt.daemon660@gmail.com>
2021-10-13 21:40:42 +0100

    Fix serialization

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 const element = document.querySelector('#dm-toggle');
 
-let prefersDarkMode = window.localStorage.getItem('prefers-dark-mode');
+let prefersDarkMode = JSON.parse(window.localStorage.getItem('prefers-dark-mode'));
 if (prefersDarkMode === null) {
   prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 }
 if (prefersDarkMode) {
   document.body.classList.add('dark');
-} else {
+} else {w
   document.body.classList.remove('dark');
 }
 element.addEventListener('click', () => {
   document.body.classList.toggle('dark');
   prefersDarkMode = !prefersDarkMode;
-  window.localStorage.setItem('prefers-dark-mode', prefersDarkMode);
+  window.localStorage.setItem('prefers-dark-mode', JSON.stringify(prefersDarkMode));
 });
