b7b888abce91ecbc1abca8cbc6f63e8d3ea1149f
Matt Smith <matt.daemon660@gmail.com>
2021-10-13 21:37:46 +0100

    Fix always darkmode

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
-const element = document.querySelector("#dm-toggle");
+const element = document.querySelector('#dm-toggle');
 
-let prefersDarkMode =
-  window.localStorage.getItem("prefers-dark-mode") ||
-  window.matchMedia("(prefers-color-scheme: dark)").matches;
-if (prefersDarkMode) {
-  document.body.classList.add("dark");
-} else {
-  document.body.classList.remove("dark");
+let prefersDarkMode = window.localStorage.getItem('prefers-dark-mode');
+if (prefersDarkMode === null) {
+  prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 }
-element.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
+if (prefersDarkMode) {
+  document.body.classList.add('dark');
+} else {
+  document.body.classList.remove('dark');
+}
+element.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
   prefersDarkMode = !prefersDarkMode;
-  window.localStorage.setItem("prefers-dark-mode", prefersDarkMode);
+  window.localStorage.setItem('prefers-dark-mode', prefersDarkMode);
 });
