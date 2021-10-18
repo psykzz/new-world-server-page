84ec6c293299ce134c013b1dc63d4d32b5491364
psykzz <psykzz@users.noreply.github.com>
2021-10-18 13:59:07 +0000

    deploy: 47f6434be8af30352313b3909180b185b8eca67d

---
+++ component---src-pages-discord-jsx-989465898cfc0879dc6a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkishtakar=self.webpackChunkishtakar||[]).push([[281],{9532:function(e,t,n){n.r(t);var r=n(7294);t.default=function(){return r.useEffect((function(){window.location.href="https://discord.gg/wg8ybKYzKK"}),[]),r.createElement(r.Fragment,null,r.createElement("meta",{"http-equiv":"refresh",content:"0; URL=https://discord.gg/wg8ybKYzKK"}),r.createElement("link",{rel:"canonical",href:"https://discord.gg/wg8ybKYzKK"}))}}}]);
+//# sourceMappingURL=component---src-pages-discord-jsx-989465898cfc0879dc6a.js.map
