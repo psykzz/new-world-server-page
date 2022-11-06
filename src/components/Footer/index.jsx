ff82bd47df2268f1d975fd8e00c8cc537f4ea752
Matt <psykzz@users.noreply.github.com>
2022-11-06 15:06:16 +0000

    Update index.jsx

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import * as styles from "./styles.module.css"
 
 export const Footer = () => (
   <div className={styles.footer}>
-    <a href="https://github.com/psykzz/ishtakar?utm_source=psykzz.dev">source</a> | made by psykzz<br />
+    <a href="https://github.com/psykzz/new-world-server-page?utm_source=psykzz.dev">source</a> | made by psykzz<br />
     data provided by <a href="https://nwdb.info?utm_source=psykzz.dev">nwdb.info</a> &hearts;
   </div>
 )
