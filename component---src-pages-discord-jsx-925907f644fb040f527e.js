8cffdf2f892fac158c3ec7d2ee7c5f2b4fad0503
psykzz <psykzz@users.noreply.github.com>
2022-12-08 20:55:41 +0000

    deploy: bbbc32d6541494cfe40506a3ea4070b0bb57d474

---
+++ component---src-pages-discord-jsx-925907f644fb040f527e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknew_world_server_page=self.webpackChunknew_world_server_page||[]).push([[281],{9796:function(e,n,t){t.r(n);var r=t(7294),a="https://discord.gg/sZSa8Eh6yq";n.default=function(){return r.useEffect((function(){window.location.href=a}),[]),r.createElement(r.Fragment,null,r.createElement("meta",{"http-equiv":"refresh",content:"0; URL="+a}),r.createElement("link",{rel:"canonical",href:a}))}}}]);
+//# sourceMappingURL=component---src-pages-discord-jsx-925907f644fb040f527e.js.map
